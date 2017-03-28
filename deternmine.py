3cb54691dc2aeed7977208ca594d21efd78c8a7a
zyqu <zhengyangqu2017@u.northwestern.edu>
2017-03-27 21:43:40 -0500

    deternmine added

---
+++ deternmine.py
@@ -0,0 +1,28 @@
+import sys, os
+
+CUR_PATH = os.path.dirname(os.path.realpath(__file__))
+LOG_PATH = os.path.join(CUR_PATH, "log")
+OUT_PATH = os.path.join(CUR_PATH, "out")
+
+if __name__=="__main__":
+	if not os.path.exists(LOG_PATH):
+		print "File not exists %s"%LOG_PATH
+		sys.exit(-1)
+	
+	try:
+		thres = float(sys.argv[1])
+	except:
+		print "usage : python deternmine.py threshold[float]"
+		sys.exit(-1)		
+
+	with open(LOG_PATH, 'r') as fin:
+		with open(OUT_PATH, 'w') as fout:
+			for l in fin.readlines():
+				l = l.strip()
+				tokens = l.split("\t")
+				fpath = tokens[0]
+				val = float(tokens[1])
+				if val > thres:
+					fout.write("%s %s\n"%(fpath, "walk"))
+				else:
+					fout.write("%s %s\n"%(fpath, "sit"))
